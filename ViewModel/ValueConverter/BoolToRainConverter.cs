3166ac85409d029be6dcfae046f8eea7625dfee5
abdullahrefaat <abdullahrefaat@gmail.com>
2024-08-27 12:21:14 +0300

    Add project files.

---
+++ ViewModel/ValueConverter/BoolToRainConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WeatherAppThree.ViewModel.ValueConverter
+{
+    public class BoolToRainConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            bool isRaining = (bool)value;
+            if (isRaining)
+                return "Currently raining";
+            return "Currently not raining";
+        }
+        
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            string isRaining = (string)value;
+            if (isRaining == "Currently raining")
+                return true;
+            return false;
+        }
+    }
+}
---
+++ ViewModel/WeatherVM.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherAppThree.Model;
+using WeatherAppThree.ViewModel.Commands;
+using WeatherAppThree.ViewModel.Helpers;
+
+namespace WeatherAppThree.ViewModel
+{
+    public class WeatherVM : INotifyPropertyChanged
+    {
+		private string query;
+
+		public string Query
+		{
+			get { return query; }
+			set 
+			{
+				query = value;
+				OnPropertyChanged("Query");
+			}
+		}
+
+
+		private CurrentConditions currentConditions;
+
+		public CurrentConditions CurrentConditions
+		{
+			get { return currentConditions; }
+			set 
+			{ 
+				currentConditions = value;
+				OnPropertyChanged("CurrentConditions");
+			}
+		}
+
+		private City selectedCity;
+
+		public City SelectedCity
+		{
+			get { return selectedCity; }
+			set 
+			{ 
+				selectedCity = value;
+				OnPropertyChanged("SelectedCity");
+				GetCurrentConditions();
+			}
+		}
+
+		public SearchCommand SearchCommand { get; set; }
+
+        public ObservableCollection<City> Cities { get; set; }
+
+        public WeatherVM()
+		{
+			if (DesignerProperties.GetIsInDesignMode(new System.Windows.DependencyObject()))
+			{
+                SelectedCity = new City
+                {
+                    LocalizedName = "New York"
+                };
+                CurrentConditions = new CurrentConditions
+                {
+                    WeatherText = "Partly Cloud",
+                    Temperature = new Temperature
+                    {
+                        Metric = new Units
+                        {
+                            Value = "21"
+                        }
+                    }
+                };
+            }
+			SearchCommand = new SearchCommand(this);
+			Cities = new ObservableCollection<City>();
+		}
+		private async void GetCurrentConditions()
+		{
+			Query = string.Empty;
+			Cities.Clear();
+			CurrentConditions = await AccWeatherHelper.GetCurrentConditions(SelectedCity.Key);
+		}
+
+		public async void MakeQuery()
+        {
+            var cities = await AccWeatherHelper.GetCities(Query);
+
+            Cities.Clear();
+            foreach (var city in cities)
+            {
+                Cities.Add(city);
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+		public void OnPropertyChanged(string propertyName)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+
+	}
+}
